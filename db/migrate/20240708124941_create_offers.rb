d16c0fb56cdbf731242a2d0ff01c0c3d6e32208c
Louis Cano <louis.cano@hotmail.fr>
2024-07-08 15:13:32 +0200

    added fields to model offer (miss disponibility)

--- db/migrate/20240708124941_create_offers.rb
+++ db/migrate/20240708124941_create_offers.rb
@@ -1,8 +1,11 @@
 class CreateOffers < ActiveRecord::Migration[7.1]
   def change
     create_table :offers do |t|
-
+      t.string "name", null: false
+      t.text "description", null: false
+      t.float "price", null: false
       t.timestamps
     end
+    add_foreign_key "users", "offers"
   end
 end
